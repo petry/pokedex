ab77f8e49d731c95ca8bbdb18426288a8775c780
Marcos Daniel Petry <marcospetry@gmail.com>
2016-08-09 22:45:49 -0300

    add templates

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+from apps.pokemon.views import PokemonView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html')),
-    url(r'^accounts/profile/$', TemplateView.as_view(template_name='profile.html')),
+    url(r'^accounts/profile/$', PokemonView.as_view(template_name='profile.html')),
     url(r'^accounts/', include('allauth.urls')),
 
 ]
